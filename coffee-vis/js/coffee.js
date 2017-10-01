22ba16a1c65179f8ffcb2c067ba8a3557982ea24
michellemxm <michelle.maxm@gmail.com>
2017-10-01 15:46:50 -0400

    15:46 oct01

--- coffee-vis/js/coffee.js
+++ coffee-vis/js/coffee.js
@@ -1,6 +1,27 @@
 var chart;
 var height = 200;
 var width = 300;
+//Sum of sales by regions
+var salesCentral = 0;
+var salesEast = 0;
+var salesWest = 0;
+var salesSouth = 0;
+//Sum of sales by categories
+var salesCoffee = 0;
+var salesEspresso = 0;
+var salesTea = 0;
+var salesHerbal = 0;
+//Sum of profit by regions
+var profitCentral = 0;
+var profitEast = 0;
+var profitWest = 0;
+var profitSouth = 0;
+//Sum of profit by categories
+var profitCoffee = 0;
+var profitEspresso = 0;
+var profitTea = 0;
+var profitHerbal = 0;
+
 //DEFINE YOUR VARIABLES UP HERE
 
 
@@ -9,12 +30,20 @@ function init(){
   chart = d3.select('#vis').append('svg');
   vis = chart.append('g');
   //PUT YOUR INIT CODE BELOW
-  d3.csv("./data/CoffeeData.csv", function(data){
-    data.forEach(function(d){
+  
+  //Count number of lines in the csv data sheet
+  //4248 rows of data for this sheet
+
+  // Parse csv and change strings to numbers
+  d3.csv("./data/CoffeeData.csv", function(data) {
+    data.forEach(function(d) {
       d.sales = +d.sales;
       d.profit = +d.profit;
+
     })
     console.log(data[0]);
+    console.log(data[1]);
+    console.log(data[2]);
   })
 
 }
