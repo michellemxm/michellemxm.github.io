4250e4039627c05289a6526cc8b608cfb02eb8d1
michellemxm <michelle.maxm@gmail.com>
2019-02-17 13:50:00 -0500

    update toggle btn

---
+++ assets/js/hide.js
@@ -0,0 +1,17 @@
+function hideToggle() {
+  	var x = document.getElementById("researchdShow");
+ 	 if (x.style.display === "none") {
+  	  x.style.display = "block";
+ 	 } else {
+  	  x.style.display = "none";
+ 	 }
+}
+
+function hideToggleTwo() {
+  	var x = document.getElementById("findingShow");
+ 	 if (x.style.display === "none") {
+  	  x.style.display = "block";
+ 	 } else {
+  	  x.style.display = "none";
+ 	 }
+}
